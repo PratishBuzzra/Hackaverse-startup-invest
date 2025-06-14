49f8d8ca0c08cc0c2f044ceaa929b259d6c5c4c1
lasata manandhar <samyak11manandhar@gmail.com>
2025-06-14 14:54:24 +0545

    navbar-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './samyak/components/navbar/navbar';
 
 function App() {
   return (
     <div className="App">
-    <Navbar /> 
-    <BrowserRouter>   
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-      </Routes>
+    <BrowserRouter>     
+      <Navbar />
+        <Routes>
+        </Routes>
     </BrowserRouter>
     </div>
     );
